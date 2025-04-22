b186b35c5bbc60761285cc0b561a4bc805702d1f
lmalmud <lmalmud2@jh.edu>
2025-04-22 08:02:14 -0400

    set up LinearApproximation structure

--- LinearApproximation.py
+++ LinearApproximation.py
@@ -1,10 +1,18 @@
 from Verticie import Verticie
 from Edge import Edge
 from Graph import Graph
+from dijkstra import *
 
 class LinearApproximation:
 
     def __init__(self, points, alpha, beta):
+        '''
+        Initializes a LinearApproximation object by constructing the corresponding graph.
+        points: two-dimensional array of points: [[x1, y1], [x2, y2], ..., [xn, yn]]
+        alpha: the additional incured cost of storage
+        beta: the additional icured cost of error between true value and approximation
+        '''
+
         self.points = points
         self.alpha = alpha # The cost of storing a segment
         self.beta = beta # The cost of error
@@ -21,3 +29,26 @@ class LinearApproximation:
                 cost = alpha
                 # FIXME: calculate the cost of this edge
                 edges.append(Edge(v_i, v_j))
+
+    def plt(self, pts):
+        '''
+        Plots the piecewise linear approximation described by the
+        data contained in each Verticie object.
+        pts: array of Verticie objects
+        '''
+        pass
+
+    def approximate(self):
+        '''
+        Runs Dijkstra's algorithm on the underlying graph in order to determine the new approximation.
+        returns: array of Verticie objects representing the points chosen to be in the approximation
+        '''
+        pass
+
+    def animate_dijkstra(self, pts_approx):
+        '''
+        Plots the points describing the original function and draws line segments between
+        each consecutive pair of points in pts_approx.
+        pts_approx: array of Verticie objects
+        '''
+        pass
---
+++ driver.py
@@ -0,0 +1,4 @@
+from LinearApproximation import LinearApproximation
+
+test1 = [[0, 0], [1, 2], [3, -1], [4, 0], [6, 2]]
+test1_obj = LinearApproximation(test1)
