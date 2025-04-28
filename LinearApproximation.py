90142cf8fd4f77353216f5e05ee7b06a5569b2f9
lmalmud <lmalmud2@jh.edu>
2025-04-28 12:59:30 -0400

    added evaluation of piecewise functions

--- LinearApproximation.py
+++ LinearApproximation.py
@@ -1,4 +1,4 @@
-from Verticie import Verticie
+from Vertice import Vertice
 from Edge import Edge
 from Graph import Graph
 from dijkstra import *
@@ -13,22 +13,71 @@ class LinearApproximation:
         beta: the additional icured cost of error between true value and approximation
         '''
 
-        self.points = points
+        if len(points) < 2:
+            raise Exception("Must include at least two points in order to define a piecewise function")
+
+        self.points = sorted(points) # Python automatically sorts multi-dimensional arrays by the first element
         self.alpha = alpha # The cost of storing a segment
         self.beta = beta # The cost of error
 
         # Create a vertex for each point
         vertices = []
         for index, point in enumerate(points):
-            vertices.append(Verticie(point[0], point[1], index))
+            vertices.append(Vertice(point[0], point[1], index))
+
         edges = []
-        
         # Insert an edge (i, j) for each i < j representing adding a segment between a_i, a_j
         for i, v_i in enumerate(vertices):
             for j, v_j in enumerate(vertices):
                 cost = alpha
                 # FIXME: calculate the cost of this edge
                 edges.append(Edge(v_i, v_j))
+
+    def f_1(self, x):
+        ''' Evalutes f_1(x), where f_1 is the true function
+        defined by the points
+        x: real number
+        Returns: real number'''
+
+        # Ensure that point we wish to evaluate the funciton at is within the domain
+        if x < self.points[0][0]: # Below the lowest point
+            raise Exception(f'x-coordinate too small: must be within the function domain of: [{self.points[0][0]}, {self.points[-1][0]}]')
+        if x > self.points[-1][0]: # Above the largest point
+            raise Exception(f'x-coordinate too large: must be within the function domain of: [{self.points[0][0]}, {self.points[-1][0]}]')
+        
+        index = 0
+        before_point = self.points[index]
+        while before_point[0] < x:
+            index += 1
+            before_point = self.points[index]
+        after_point = self.points[-1]
+        if index < len(self.points) - 1: # The point is not the last point
+            after_point = self.points[index+1]
+        else: # In this case, x is the last point, so use the last segment
+            before_point = self.points[index-1]
+
+        # To debug the boundaries:
+        # print(f'x: {x}, before_point: {before_point}, after_point: {after_point}')
+
+        # The main amount of work in this function is to determine the approproate
+        # points that define the segment that x is located in.
+        return self.eval_segment(x, before_point, after_point)
+
+    def eval_segment(self, x, pt1, pt2):
+        ''' Returns the value of the linear function defined by the line through
+        p1 and p2
+        x: real number
+        pt1:[x1, y1]
+        pt2: [x2, y2]
+        f(x) = (y2-y1)/(x2-x1)(x - x1) + y1
+        Returns: real number
+        '''
+        x1 = pt1[0]
+        y1 = pt1[1]
+        x2 = pt2[0]
+        y2 = pt2[1]
+        m = (y2-y1)/(x2-x1)
+        return (m * (x - x1)) + y1
 
     def plt(self, pts):
         '''
--- driver.py
+++ driver.py
@@ -1,4 +1,15 @@
 from LinearApproximation import LinearApproximation
 
 test1 = [[0, 0], [1, 2], [3, -1], [4, 0], [6, 2]]
-test1_obj = LinearApproximation(test1)
+test1_obj = LinearApproximation(test1, 1, 1)
+
+print('TEST: evaluate_segment')
+print(f'evaluate segment between (0, 0), (1, 2) at 0 expects 0: {test1_obj.eval_segment(0, test1[0], test1[1])}')
+print(f'evaluate segment between (0, 0), (1, 2) at .5 expects 1: {test1_obj.eval_segment(.5, test1[0], test1[1])}')
+print(f'evaluate segment between (0, 0), (1, 2) at 1 expects 2: {test1_obj.eval_segment(1, test1[0], test1[1])}')
+
+print('TEST: f_1')
+print(f'evaluate f_1(.5) expects 1: {test1_obj.f_1(.5)}')
+print(f'evaluate f_1(3.5) expects -.5: {test1_obj.f_1(3.5)}')
+print(f'evaluate f_1(0) expects 0 (first point - edge case): {test1_obj.f_1(0)}')
+print(f'evaluate f_1(6) expects 2 (last point - edge case): {test1_obj.f_1(6)}')
